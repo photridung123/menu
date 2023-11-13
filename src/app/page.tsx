71cf385e52732a2c21ec5b96f4504947f216a66e
photridung123 <55821539+photridung123@users.noreply.github.com>
2023-11-13 20:27:50 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps
 
 const Toast = ({ msg }: { msg: string }) => {
   return (
-    <div className="font-bold fixed z-50 left-0 bottom-10 w-2/3 h-fit bg-amber-200/90 rounded-tl-md rounded-tr-md grid place-items-center p-4">
+    <div className="font-bold fixed z-50 left-0 bottom-10 w-2/3 h-1/5 bg-amber-200/90 rounded-tl-md rounded-tr-md grid place-items-center p-4">
       {msg}
     </div>
   );
